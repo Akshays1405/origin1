3043f8e3744096aeee877f63fa0a2863f985bb35
Akshays1405 <akshays1405@gmail.com>
2023-06-19 10:29:18 +0530

    commit changes

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -5,10 +5,15 @@ test('home page',async ({page})=>{
     await page.goto('https://www.facebook.com/')
 
     let pageTitle= page.title();
-    console.log('page title is', pageTitle)
+    console.log('page title is', pageTitle);
+    await expect(page).toHaveTitle('facebook');
 
-  await  expect(page).toHaveTitle('facebook');
+    let pageURL=page.url();
+    console.log('Page URL is:',pageURL);
+
+    await  expect(page).toHaveTitle('facebook');
   await expect(page).toHaveURL('https://www.facebook.com/');
     await page.close();
+
 
 } )
